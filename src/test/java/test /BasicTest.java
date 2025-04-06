3a0bf89de36f6e7a77e6d80046a160cd6a8e2959
chauhan-him <95032968+chauhan-him@users.noreply.github.com>
2025-04-06 22:50:45 +0530

    Create BasicTest.java

---
+++ src/test/java/test /BasicTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BasicTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
